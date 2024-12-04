fd6afdab5b90ece722b8d98d0ac5c6c50a231a77
皮皮猴 <oncwnuO3yeraKDrvhjIVObR8ugNw@git.weixin.qq.com>
2024-12-04 11:19:56 +0800

    The version that can choose products randomly, still lack error processing and final outcome check

---
+++ main.py
@@ -0,0 +1,110 @@
+import time
+import random
+from os import cpu_count
+from time import sleep
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+def xFind(xpath: str, parent_element = None):
+    if parent_element is None:
+        parent_element = driver
+    return parent_element.find_element(By.XPATH, xpath)
+
+def cFind(classname: str, parent_element = None):
+    if parent_element is None:
+        parent_element = driver
+    return parent_element.find_elements(By.CLASS_NAME, classname)
+
+def CSSFinds(classname: str, parent_element = None):
+    if parent_element is None:
+        parent_element = driver
+    return parent_element.find_elements(By.CSS_SELECTOR, classname)
+
+
+opt = Options()
+opt.add_experimental_option('detach', True)
+driver = webdriver.Chrome(service=Service('chromedriver.exe'), options=opt)
+driver.implicitly_wait(10)
+driver.get('https://magento.softwaretestingboard.com/')
+
+
+def login():
+    # log in
+    _SignIn = xFind('/html/body/div[2]/header/div[1]/div/ul/li[2]/a')
+    print(1)
+    _SignIn.click()
+    print(2)
+    _Email = xFind('/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div[2]/form/fieldset/div[2]/div/input')
+    print(3)
+    _Password = xFind('/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div[2]/form/fieldset/div[3]/div/input')
+    print(4)
+    _Email.send_keys('zhang.23519@gmail.com')
+    print(5)
+    _Password.send_keys('123456zzZZ')
+    print(6)
+    _SignIn = xFind('//*[@id="send2"]/span')
+    print(7)
+    _SignIn.click()
+    print('login is done')
+
+
+# select area of products
+# size and color
+cloth = ['//*[@id="ui-id-11"]', '//*[@id="ui-id-12"]', '//*[@id="ui-id-13"]', '//*[@id="ui-id-14"]', '//*[@id="ui-id-19"]', '//*[@id="ui-id-20"]', '//*[@id="ui-id-21"]', '//*[@id="ui-id-22"]']
+times = random.randint(1,6)
+all_items = []
+print(times)
+for j in range(times):
+    driver.execute_script("arguments[0].click();", xFind( random.choice(cloth) ))
+
+    # select clothes
+    # class: product-item-photo
+
+    items = []
+    numbers = random.randint(1, 10)
+    print(numbers)
+    for i in range(numbers):
+        products = driver.find_elements(By.CLASS_NAME, 'product-image-photo')
+        product = random.choice(products)
+
+        driver.execute_script("arguments[0].click();", product)
+
+        _Sizes = driver.find_elements(By.CSS_SELECTOR, 'div.swatch-option.text')
+        _Size = random.choice(_Sizes)
+        size = _Size.text
+        driver.execute_script("arguments[0].click();", _Size)
+
+        _Colors = driver.find_elements(By.CSS_SELECTOR, 'div.swatch-option.color')
+        _Color = random.choice(_Colors)
+        color = _Color.get_dom_attribute("option-label")
+        driver.execute_script("arguments[0].click();", _Color)
+
+        _Qty = driver.find_element(By.CSS_SELECTOR, '.input-text.qty')
+        _Qty.clear()
+        num = random.randint(1, 100) # 先暂时都给正确的数字，后面再处理错误
+        _Qty.send_keys(str(num))
+
+        _Price = driver.find_element(By.CSS_SELECTOR, '[data-price-type="finalPrice"]')
+        price = float(_Price.get_dom_attribute('data-price-amount'))
+
+        _Photo = driver.find_element(By.CSS_SELECTOR, 'img[aria-hidden="false"]')
+        print(_Photo)
+        src = _Photo.get_dom_attribute('src')
+
+        items.append((src, size, color, num, price))
+        print(items[-1])
+
+        span_element = driver.find_element(By.XPATH, '//span[normalize-space(text())="Add to Cart"]')
+        driver.execute_script("arguments[0].click();", span_element)
+
+
+        driver.back()
+    all_items.append(items)
+
+print (all_items)
